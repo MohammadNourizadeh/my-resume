47c373170216369e00e5288e3118ced603848d4d
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2025-06-02 13:16:40 +0330

    first infochanged

--- src/pages/homePage/components/firstInfo/FirstInfo.tsx
+++ src/pages/homePage/components/firstInfo/FirstInfo.tsx
@@ -5,7 +5,7 @@ export default function FirstInfo() {
         <div className={styles.king}>
             <div className={styles.myName}>mohammad nourizadeh</div>
             <p className={styles.feDeveloper}>front-end developer</p>
-            <p className={styles.description}>i am a front-end developer with experience in building modern web application</p>
+            <p className={styles.description}>Front-end developer focused on building modern, user-friendly web apps and improving skills.</p>
         </div>
     )
 }
